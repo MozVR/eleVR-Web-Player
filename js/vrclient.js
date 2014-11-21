8ee3ae99053c4785d5cd7a1de80fc46e50ad66ff
Casey Yee <caseyyee.ca@gmail.com>
2014-11-20 16:20:30 -0800

    updated vrclient, added site meta

--- js/vrclient.js
+++ js/vrclient.js
@@ -1,62 +1,9 @@
 window.VRClient = (function() {
-
-  //used by camera and head-tracking elements
-  var baseTransform = "translate3d(0, 0, 0) rotateZ(180deg) rotateY(180deg)";
-
-  // helper function to convert a quaternion into a matrix, optionally
-  // inverting the quaternion along the way
-  function matrixFromOrientation(q, inverse) {
-    var m = Array(16);
-
-    var x = q.x, y = q.y, z = q.z, w = q.w;
-
-    // if inverse is given, invert the quaternion first
-    if (inverse) {
-      x = -x; y = -y; z = -z;
-      var l = Math.sqrt(x*x + y*y + z*z + w*w);
-      if (l === 0) {
-        x = y = z = 0;
-        w = 1;
-      } else {
-        l = 1/l;
-        x *= l; y *= l; z *= l; w *= l;
-      }
-    }
-
-    var x2 = x + x, y2 = y + y, z2 = z + z;
-    var xx = x * x2, xy = x * y2, xz = x * z2;
-    var yy = y * y2, yz = y * z2, zz = z * z2;
-    var wx = w * x2, wy = w * y2, wz = w * z2;
-
-    m[0] = 1 - (yy + zz);
-    m[4] = xy - wz;
-    m[8] = xz + wy;
-
-    m[1] = xy + wz;
-    m[5] = 1 - (xx + zz);
-    m[9] = yz - wx;
-
-    m[2] = xz - wy;
-    m[6] = yz + wx;
-    m[10] = 1 - (xx + yy);
-
-    m[3] = m[7] = m[11] = 0;
-    m[12] = m[13] = m[14] = 0;
-    m[15] = 1;
-
-    return m;
-  }
-
-  function cssMatrixFromElements(e) {
-    return "matrix3d(" + e.join(",") + ")";
-  }
-
-  function cssMatrixFromOrientation(q, inverse) {
-    return cssMatrixFromElements(matrixFromOrientation(q, inverse));
-  }
-
-  function VRClient(container) {
+  function VRClient() {
     var self = this;
+
+    // call back for render mode changes.
+    self.onRenderModeChange = null;
 
     // this promise resolves when VR devices are detected.
     self.getVR = new Promise(function (resolve, reject) {
@@ -92,18 +39,64 @@ window.VRClient = (function() {
       self.startDemo = resolve;
     });
 
+    self.sendMessage('loading', self.getPageMeta());
+
+    // listen for any post messages
     window.addEventListener("message", function (e) {
       var msg = e.data;
       if (!msg.type) {
         return;
       }
+
+      //console.log('message received ', msg.type, msg.data);
+
       switch (msg.type) {
         case 'start':
           self.startDemo();
           break;
+        case 'renderMode':
+          self.setRenderMode(msg.data);
+          break;
+        case 'onZeroSensor':
+          self.zeroSensor();
+          break;
+        case 'onBlur':
+          if (typeof self.onBlur == 'function') {
+            self.onBlur();
+          }
+          break;
+        case 'onFocus':
+          if (typeof self.onFocus == 'function') {
+            self.onFocus();
+          }
+          break;
       }
     }, false);
   }
+
+  VRClient.prototype.getPageMeta = function() {
+    // capture page meta
+    // title tag
+    var title;
+    try {
+      title = document.getElementsByTagName('title')[0].textContent;
+    } catch(e) {
+      title = undefined;
+    }
+
+    // description meta
+    var description;
+    try {
+      description = document.querySelector("meta[name=\'description\']").content;
+    } catch(e) {
+      description = undefined;
+    }
+
+    return {
+      title: title,
+      description: description
+    }
+  };
 
   VRClient.prototype.sendMessage = function (type, data) {
     if (window.parent !== window) {
@@ -114,8 +107,15 @@ window.VRClient = (function() {
     }
   };
 
-  VRClient.prototype.load = function (url) {
-    this.sendMessage('load', url);
+  VRClient.prototype.load = function (url, opts) {
+    if (!opts) {
+      opts = {}
+    }
+
+    this.sendMessage('load', {
+      url: url,
+      opts: opts
+    });
   };
 
   // Takes value 0..1 to represent demo load progress. Optional.
@@ -129,20 +129,33 @@ window.VRClient = (function() {
     return this.wait;
   };
 
-  // if this demo has an exit
+  // if this demo has an completed and we can shit it down.
   VRClient.prototype.ended = function() {
     this.sendMessage('ended');
-  }
-
-  VRClient.prototype.getVR = function () {
-    return this.getVR;
   };
+
 
   VRClient.prototype.zeroSensor = function () {
     var self = this;
     self.getVR.then(function () {
-      self.positionDevice.zeroSensor();
+      if (typeof self.onZeroSensor == 'function') {
+        self.onZeroSensor();
+      }
     });
+  };
+
+  VRClient.prototype.setRenderMode = function(mode) {
+    var self = this;
+
+    if (typeof self.onRenderModeChange == 'function') {
+      self.onRenderModeChange(mode);
+    }
+  };
+
+  VRClient.renderModes = VRClient.prototype.renderModes = {
+    mono: 1,
+    stereo: 2,
+    vr: 3
   };
 
   return new VRClient();
